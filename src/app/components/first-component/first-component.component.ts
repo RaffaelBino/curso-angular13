399c7e16a0499ec8a1286a079a2d41c278788c93
Raffael Bino <raffael_bino18@yahoo.com.br>
2022-03-24 21:12:57 -0300

    Adicionando Arquivos do curso

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-first-component',
   templateUrl: './first-component.component.html',
-  styleUrls: ['./first-component.component.css']
+  styleUrls: ['./first-component.component.css'],
 })
 export class FirstComponentComponent implements OnInit {
+  name: string = 'Raffael';
+  age: number = 30;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
