b0a049941f2083443d4c68ba36a244e7a8a3ec9e
Raffael Bino <raffael_bino18@yahoo.com.br>
2022-03-29 22:15:47 -0300

    Aula de Routes

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { ListRenderComponent } from './components/list-render/list-render.component';
+import { TwoWayBindingComponent } from './components/two-way-binding/two-way-binding.component';
+import { EventosComponent } from './components/eventos/eventos.component';
+
+const routes: Routes = [
+  { path: '', component: FirstComponentComponent },
+  { path: 'list', component: ListRenderComponent },
+  { path: '2waydb', component: TwoWayBindingComponent },
+  { path: 'eventos', component: EventosComponent },
+];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
